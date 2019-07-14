4679bec589ff4665d3afeacd8ca996076c5bbfd5
Ravikiran303 <ravikiran2221@gmail.com>
2019-07-14 16:23:39 +0530

    Code format

--- taskbackend/app.js
+++ taskbackend/app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
 
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -19,63 +19,61 @@ app.use(
 const cors = require("cors");
 app.use(cors());
 
-const User =require('./models/User');
+const User = require("./models/User");
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.post('/user/Register',(req,res) =>{
-    const details = {
-        userID : req.body.userID,
-        password : req.body.password,
-        userName : req.body.userName,
-        department : req.body.department
-    }
-    User.findOne({userID : req.body.userID}).then(newuser => {
-        if (!newuser) {
-          bcrypt.hash(req.body.password, 10, (err, hash) => {
-            details.password = hash;
-            User.create(details)
-              .then(newuser => {
-                res.json({ status: newuser.userID + " registered" });
-              })
-              .catch(err => {
-                res.send(err);
-              });
+app.post("/user/Register", (req, res) => {
+  const details = {
+    userID: req.body.userID,
+    password: req.body.password,
+    userName: req.body.userName,
+    department: req.body.department
+  };
+  User.findOne({ userID: req.body.userID }).then(newuser => {
+    if (!newuser) {
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        details.password = hash;
+        User.create(details)
+          .then(newuser => {
+            res.json({ status: newuser.userID + " registered" });
+          })
+          .catch(err => {
+            res.send(err);
           });
-        } else {
-          res.json({ error: err });
-        }
       });
+    } else {
+      res.json({ error: err });
+    }
+  });
 });
 process.env.SECRET_KEY = "secret";
 app.post("/user/login", (req, res) => {
-    User.findOne({
-      userID: req.body.userID
+  User.findOne({
+    userID: req.body.userID
+  })
+    .then(user => {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        const payload = {
+          _id: user._id,
+          userID: user.userID
+        };
+        let token = jwt.sign(payload, process.env.SECRET_KEY, {
+          expiresIn: 3220
+        });
+        res.send({ auth_token: token });
+      } else {
+        res.status(401).json({ error: "User does not exist" });
+      }
     })
-      .then(user => {
-        if (bcrypt.compareSync(req.body.password, user.password)) {
-          
-          const payload = {
-            _id: user._id,
-            userID: user.userID
-          };
-          let token = jwt.sign(payload, process.env.SECRET_KEY, {
-            expiresIn: 3220
-          });
-          res.send(user.userID + "Successfully loggedin");
-        } else {
-          res.status(401).json({ error: "User does not exist" });
-        }
-      })
-      .catch(err => {
-        res.status(404).send("error" + err);
-      });
-  });
-
+    .catch(err => {
+      res.status(404).send("error" + err);
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- taskbackend/models/User.js
+++ taskbackend/models/User.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const User_Schema = new Schema({
-    userID : { type: String, required: true },
-    userName: { type: String, required: true },
+  userID: { type: String, required: true },
+  userName: { type: String, required: true },
   password: { type: String, required: true },
-  department: {type: String,required: true}
-  });
-  module.exports = User = mongoose.model("Users", User_Schema, "Users");
+  department: { type: String, required: true }
+});
+module.exports = User = mongoose.model("Users", User_Schema, "Users");
