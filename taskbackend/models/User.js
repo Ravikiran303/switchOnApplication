dab79a37f2bd9d6b98f87d61bb2d2a87ff822406
Ravikiran303 <ravikiran2221@gmail.com>
2019-07-14 12:44:28 +0530

    Create user schema

---
+++ taskbackend/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const User_Schema = new Schema({
+    userID : { type: String, required: true },
+    userName: { type: String, required: true },
+  password: { type: String, required: true },
+  department: {type: String,required: true}
+  });
+  module.exports = User = mongoose.model("Users", User_Schema, "Users");
